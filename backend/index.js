0f303891c8ef53916b96f3429e6853d325a628ad
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-03 12:19:40 +0300

    backend - connect to mysql db

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,26 @@
 import express from "express";
+import mysql from "mysql";
 
 const app = express();
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "bookkeeper",
+});
+
+app.get("/", (req, res) => {
+  res.json("hello from backend");
+});
+
+app.get("/books", (req, res) => {
+  const q = "SELECT * FROM books";
+  db.query(q, (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
 
 // port
 const PORT = process.env.PORT || 5000;
