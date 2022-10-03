895fa5381c182946386f431d22e35800ccd9d19d
Omar Zeineddine <ozeineddine.dev@gmail.com>
2022-10-03 15:29:20 +0300

    feat - fetch all books

--- frontend/src/pages/Books.jsx
+++ frontend/src/pages/Books.jsx
@@ -1,6 +1,20 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const Books = () => {
+  const [books, setBooks] = useState([]);
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get("http://localhost:5000/books");
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
   return <div>Books</div>;
 };
 
